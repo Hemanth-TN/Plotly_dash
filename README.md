7c510cba5eb169ee0f021d4d3d72ee25bfbc389c
Hemanth Thandaga Nagaraju <hemanth.tnagaraju@gmail.com>
2024-12-23 07:28:45 -0600

    added the Output_static_images.pptx and requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This repo contains exercises I tried to build dash apps using plotly dash through online sources.
+The requirements file is attached
+
+The screenshots of the app are provided in Output_static_images.pptx
--- session_2_exA.py
+++ session_2_exA.py
@@ -9,9 +9,36 @@ from dash import dcc, html, Input, Output, Dash
 df = pd.read_csv("./Data/2011_us_ag_exports.csv")
 
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
+                meta_tags=[{'name': 'viewport',
+                            'content': 'width=device-width ,initial-scale=1.0'}])
 
-app.layout = 
+
+
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(html.H1(children='Statewise Aggregation of Food exports in US',
+                        id='my-title'), class_name='text-center my-5 text-primary')
+            ]),
+    dbc.Row([
+        dbc.Col([dcc.Dropdown(id='state-dropdown',
+                              multi=True,
+                              value=['Alabama','Arkansas'],
+                              options=[{'label':x, 'value':x} for x in df['state'].unique()]),
+                dcc.Graph(id='graph1', figure={})
+                              ])
+    ])
+])
+
+
+@app.callback(
+    Output(component_id='graph1', component_property='figure'),
+    [Input(component_id='state-dropdown', component_property='value')]
+)
+def update_bar(state_selected):
+    df_country = df.loc[df['state'].isin(state_selected)].copy()
+    fig = px.bar(df_country, x='state',y=['beef','pork','fruits fresh'])
+    return fig
 
 
 
